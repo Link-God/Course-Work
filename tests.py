a7c5a63685e1ab6f552f3fe6913523619c23f157
Yury <32190920+Link-God@users.noreply.github.com>
2018-12-15 11:43:36 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,23 +2,24 @@ import unittest
 import Game
 
 b_for_ex = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 1, 0, 1, 1, 1, 1],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 1, 1, 1, 0, 0, 1, 0, 0]
-        ]
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 1, 1, 1, 1],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 0, 0, 1, 0, 0]
+]
+
 
 class TestStringMethods(unittest.TestCase):
-    def test_init(self):
+    def test_one(self):
         g = Game.Computer()
         self.assertEqual(g.player_counter, 0)
-  
+
     def test_receive_shot(self):
         success1, c11, c12 = Game.receive_shot('a1')
         success2, _, _ = Game.receive_shot('t5')
@@ -54,6 +55,57 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(success, False)
         length, success = Game.set_player_ships(g, 's4', d)
         self.assertEqual(success, False)
+
+    def test_free_space(self):
+        g = Game.Computer()
+        l = Game.free_space(g, 'a', 'b', 1, 1)
+        l_check = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(l, l_check)
+        l = Game.free_space(g, 'c', 'c', 3, 6)
+        l_check = [0 for _ in range(3 * 6)]
+        self.assertEqual(l, l_check)
+        d = {i: 0 for i in range(1, 5, 1)}
+        _, _ = Game.set_player_ships(g, 'a1:b1', d)
+        l = Game.free_space(g, 'a', 'a', 2, 3)
+        l_check = [0 for _ in range(4 * 3)]
+        self.assertNotEqual(l, l_check)
+
+    def test_handle_dialog(self):
+        pass
+
+    def test_end_of_game(self):
+        g = Game.Computer()
+        who = g.end_of_game()
+        who_check = 'Nobody'
+        self.assertEqual(who, who_check)
+        g.player_ships = {i: 0 for i in range(1, 5, 1)}
+        who = g.end_of_game()
+        who_check = "alice"
+        self.assertEqual(who, who_check)
+        g1 = Game.Computer()
+        g1.alice_ships = {i: 0 for i in range(1, 5, 1)}
+        who = g1.end_of_game()
+        who_check = 'player'
+        self.assertEqual(who, who_check)
+
+    def test_clear_board(self):
+        g = Game.Computer()
+        b = g.alice_board
+        self.assertFalse(2 in b)
+
+    def test_create_and_placed_ships(self):
+        g = Game.Computer()
+        b = g.alice_board
+        count = 0
+        for row in b:
+            for el in row:
+                if el == 1:
+                    count += 1
+        self.assertEqual(count, (1 * 4 + 2 * 3 + 3 * 2 + 4 * 1))
+
+    def test_player_shot(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
