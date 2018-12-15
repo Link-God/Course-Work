3838afec6e60c2934b743098a24fe71f5c4eeeef
Yury <32190920+Link-God@users.noreply.github.com>
2018-12-15 14:30:24 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,7 @@
+"""
+Tests TESTS
+"""
+
 import unittest
 import Game
 
@@ -16,7 +20,14 @@ b_for_ex = [
 
 
 class TestStringMethods(unittest.TestCase):
+    """
+    Tests for project.
+    """
     def test_one(self):
+        """
+        TEST DOC
+        :return:None
+        """
         g = Game.Computer()
         self.assertEqual(g.player_counter, 0)
 
@@ -115,64 +126,119 @@ class TestStringMethods(unittest.TestCase):
         self.assertFalse(g.player_shot(c1, c2))
         self.assertEqual(g.alice_board[0][9], 2)
 
-#     def test_req_one(self):
-#         sessionStorage = {}
-#         request = {
-#             "meta": {
-#                 "client_id": "ru.yandex.searchplugin/7.16 (none none; android 4.4.2)",
-#                 "interfaces": {
-#                     "screen": {}
-#                 },
-#                 "locale": "ru-RU",
-#                 "timezone": "UTC"
-#             },
-#             "request": {
-#                 "command": "Что ты умеешь",
-#                 "nlu": {
-#                     "entities": [],
-#                     "tokens": [
-#                         "что",
-#                         "ты",
-#                         "умеешь"
-#                     ]
-#                 },
-#                 "original_utterance": "Что ты умеешь",
-#                 "type": "SimpleUtterance"
-#             },
-#             "session": {
-#                 "message_id": 5,
-#                 "new": False,
-#                 "session_id": "766c8360-d2183428-7dc80021-6166061d",
-#                 "skill_id": "7e59ae3a-373f-4657-8987-1bd342e3b348",
-#                 "user_id": "3D95D8CAAAAEEB9E8DA84C9144C0102DDA6BEEE714EB9A6A6E747FB16C27DD5A"
-#             },
-#             "version": "1.0"
-#         }
-#         response = {
-#             "version": request['version'],
-#             "session": request['session'],
-#             "response": {
-#                 'text': 'ha',
-#                 "end_session": False
-#             }
-#         }
-#         Game.handle_dialog(request, response)
-#         check = {'version': '1.0',
-#                  'session': {'message_id': 5, 'new': False, 'session_id': '766c8360-d2183428-7dc80021-6166061d',
-#                              'skill_id': '7e59ae3a-373f-4657-8987-1bd342e3b348',
-#                              'user_id': '3D95D8CAAAAEEB9E8DA84C9144C0102DDA6BEEE714EB9A6A6E747FB16C27DD5A'},
-#                  'response': {
-#                      'text': 'Если вы еще не расставили корабли то укажите их координаты.'
-#                              ' Однопалубные можно ставить, указав лишь одну координату(Например: a1)\n'
-#                              'Корабли большей длины необходимо ставить, указывая их начальные и конечные координаты\n'
-#                              'Примеры : a1:a3 или a1:c1\n'
-#                              'Между любыми кораблями должно быть расстояние минимуум в одну клетку. '
-#                              'Нельзя ставить корабли полностью или частично за пределы поля.\n'
-#                              'Если же вы уже рассавили корабли, то вам лишь нужно написать команду- выстрел в формате буква-число. '
-#                              'Используються первые 10 букв латинсокго алфавита(abcd...). А числа от 1 до 10.\n'
-#                              'Стрелять за пределы поля и в уже подбитые корабли нельзя',
-#                      'end_session': False, 'buttons': [{'title': 'Помощь', 'hide': True}]}}
-#         self.assertEqual(response, check)
+    def test_req_one(self):
+        sessionStorage = {}
+        request = {
+            "meta": {
+                "client_id": "ru.yandex.searchplugin/7.16 (none none; android 4.4.2)",
+                "interfaces": {
+                    "screen": {}
+                },
+                "locale": "ru-RU",
+                "timezone": "UTC"
+            },
+            "request": {
+                "command": "",
+                "nlu": {
+                    "entities": [],
+                    "tokens": []
+                },
+                "original_utterance": "",
+                "type": "SimpleUtterance"
+            },
+            "session": {
+                "message_id": 0,
+                "new": True,
+                "session_id": "fbfdee58-c6137313-1406eb2c-24fda153",
+                "skill_id": "7e59ae3a-373f-4657-8987-1bd342e3b348",
+                "user_id": "3D95D8CAAAAEEB9E8DA84C9144C0102DDA6BEEE714EB9A6A6E747FB16C27DD5A"
+            },
+            "version": "1.0"
+        }
+        response = {
+            "version": request['version'],
+            "session": request['session'],
+            "response": {
+                'text': 'ha',
+                "end_session": False
+            }
+        }
+        Game.handle_dialog(request, response)
+        check1 = {'version': '1.0',
+                  'session': {'message_id': 0, 'new': True, 'session_id': 'fbfdee58-c6137313-1406eb2c-24fda153',
+                              'skill_id': '7e59ae3a-373f-4657-8987-1bd342e3b348',
+                              'user_id': '3D95D8CAAAAEEB9E8DA84C9144C0102DDA6BEEE714EB9A6A6E747FB16C27DD5A'},
+                  'response': {
+                      'text': 'Приветствую! Этот навык явялеться закрытым(он не отобрадаеться в каталоге). '
+                              'Расставь свои корабли.\nОднопалубные можно ставить лишь указав одну координату.\n'
+                              'Например так : a1 или h10.\n'
+                              'Корабли большей длины можно поставить, указав их начальные и конечный координаты через ":"\n'
+                              'Например так: a1:a3 или a1:b1\n'
+                              'Координаты указываються в виде пары буква - число\n'
+                              'Используються первые 10 букв латинсокго алфавита(abcd...). А числа от 1 до 10.\n'
+                              'В игре 4 корабля длины 1. 3 длины 2. 2 длины 3. 1 длины 4.\n'
+                              'Между любыми кораблями должно быть расстояние минимуум в одну клетку.'
+                              ' Нельзя ставить корабли полностью или частично за пределы поля.'
+                              'При использовании кнопки random корабли автоматичекси выставятся произвольным образом\nУдачи!',
+                      'end_session': False, 'buttons': [{'title': 'random', 'hide': True}]}}
+
+        self.assertEqual(response, check1)
+        request = {
+            "meta": {
+                "client_id": "ru.yandex.searchplugin/7.16 (none none; android 4.4.2)",
+                "interfaces": {
+                    "screen": {}
+                },
+                "locale": "ru-RU",
+                "timezone": "UTC"
+            },
+            "request": {
+                "command": "Что ты умеешь",
+                "nlu": {
+                    "entities": [],
+                    "tokens": [
+                        "что",
+                        "ты",
+                        "умеешь"
+                    ]
+                },
+                "original_utterance": "Что ты умеешь",
+                "type": "SimpleUtterance"
+            },
+            "session": {
+                "message_id": 5,
+                "new": False,
+                "session_id": "766c8360-d2183428-7dc80021-6166061d",
+                "skill_id": "7e59ae3a-373f-4657-8987-1bd342e3b348",
+                "user_id": "3D95D8CAAAAEEB9E8DA84C9144C0102DDA6BEEE714EB9A6A6E747FB16C27DD5A"
+            },
+            "version": "1.0"
+        }
+        response = {
+            "version": request['version'],
+            "session": request['session'],
+            "response": {
+                'text': 'ha',
+                "end_session": False
+            }
+        }
+        Game.handle_dialog(request, response)
+        check = {'version': '1.0',
+                 'session': {'message_id': 5, 'new': False, 'session_id': '766c8360-d2183428-7dc80021-6166061d',
+                             'skill_id': '7e59ae3a-373f-4657-8987-1bd342e3b348',
+                             'user_id': '3D95D8CAAAAEEB9E8DA84C9144C0102DDA6BEEE714EB9A6A6E747FB16C27DD5A'},
+                 'response': {
+                     'text': 'Если вы еще не расставили корабли то укажите их координаты.'
+                             ' Однопалубные можно ставить, указав лишь одну координату(Например: a1)\n'
+                             'Корабли большей длины необходимо ставить, указывая их начальные и конечные координаты\n'
+                             'Примеры : a1:a3 или a1:c1\n'
+                             'Между любыми кораблями должно быть расстояние минимуум в одну клетку. '
+                             'Нельзя ставить корабли полностью или частично за пределы поля.\n'
+                             'Если же вы уже рассавили корабли, то вам лишь нужно написать команду- выстрел в формате буква-число. '
+                             'Используються первые 10 букв латинсокго алфавита(abcd...). А числа от 1 до 10.\n'
+                             'Стрелять за пределы поля и в уже подбитые корабли нельзя',
+                     'end_session': False, 'buttons': [{'title': 'Помощь', 'hide': True}]}}
+        self.assertEqual(response, check)
 
     def test_req_two(self):
         sessionStorage = {}
